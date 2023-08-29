a1b55c7d3a05a15be59004063f17f3331c9e33d3
Thabiso Rankhala <93855990+Rankhala@users.noreply.github.com>
2023-08-30 00:35:56 +0200

    Delete commerce directory

--- commerce/auctions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, AuctionListings, Bid, Comment
-
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(AuctionListings)
-admin.site.register(Bid)
-admin.site.register(Comment)
